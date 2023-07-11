549ff802e88bdff1e532c609852be266c79ed536
arichorn <78001398+arichorn@users.noreply.github.com>
2023-07-11 16:29:07 -0500

    Update Header.h

--- Header.h
+++ Header.h
@@ -129,6 +129,8 @@
 @end
 
 @interface _ASDisplayView : UIView
+- (UILabel *)findLabelInSubviews:(NSArray *)subviews;
+- (void)customizeLabel:(UILabel *)label;
 @end
 
 @interface YTCommentDetailHeaderCell : UIView
