bb7990a81505c37823b96db12848ac79b35ccb9f
arichorn <78001398+arichorn@users.noreply.github.com>
2023-09-02 12:31:37 -0500

    LowContrastMode v1.4.2 Headers

--- Header.h
+++ Header.h
@@ -115,6 +115,24 @@
 @end
 
 // App Theme
+@interface YTColor : NSObject
++ (UIColor *)white1;
++ (UIColor *)white2;
++ (UIColor *)white3;
++ (UIColor *)white4;
++ (UIColor *)white5;
++ (UIColor *)black0;
++ (UIColor *)black1;
++ (UIColor *)black2;
++ (UIColor *)black3;
++ (UIColor *)black4;
++ (UIColor *)blackPure;
++ (UIColor *)grey1;
++ (UIColor *)grey2;
++ (UIColor *)white1Alpha98;
++ (UIColor *)white1Alpha95;
+@end
+
 @interface YCHLiveChatView : UIView
 @end
 
